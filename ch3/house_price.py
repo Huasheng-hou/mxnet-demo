c0e0ad4d489bf534595c42e014dbfc0e48526b09
Huasheng Hou <331292108@qq.com>
2020-06-17 18:18:06 +0800

    部分修改

--- ch3/house_price.py
+++ ch3/house_price.py
@@ -56,3 +56,33 @@ def train(net, train_features, train_labels, test_features, test_labels,
         if test_labels is not None:
             test_ls.append(log_rmse(net, test_features, test_labels))
     return train_ls, test_ls
+
+
+def get_k_fold_data(k, i, X, y):
+    assert k > 1
+    fold_size = X.shape[0] // k
+    X_train, y_train = None, None
+    for j in range(k):
+        idx = slice(j * fold_size, (j + 1) * fold_size)
+        X_part, y_part = X[idx, :], y[idx]
+        if j == i:
+            X_valid, y_valid = X_part, y_part
+        elif X_train is None:
+            X_train, y_train = X_part, y_part
+        else:
+            X_train = nd.concat(X_train, X_part)
+            y_train = nd.concat(y_train, y_part)
+    return X_train, y_train, X_valid, y_valid
+
+
+def k_fold(k, X_train, y_train, num_epochs, learning_rate,
+           weight_decay, batch_size):
+    train_l_sum, valid_l_sum = 0, 0
+    for i in range(k):
+        data = get_k_fold_data(k, i, X_train, y_train)
+        net = get_net()
+        train_ls, valid_ls = train(net, *data, num_epochs, learning_rate, weight_decay, batch_size)
+        train_l_sum += train_ls[-1]
+        valid_l_sum += valid_ls[-1]
+        if i == 0:
+            d2l.semilogy()
